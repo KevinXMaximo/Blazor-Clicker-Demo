ee1b79b59b8a0e31ead68cb3b9eab65af6daab97
KevinXMaximo <62119614+KevinXMaximo@users.noreply.github.com>
2021-10-09 11:31:43 -0400

    Add files via upload

---
+++ TodoItem.cs
@@ -0,0 +1,6 @@
+﻿    public class TodoItem // Creates a class called TodoItem, this class will be used to implement two fields into our main code
+    { // opens content of the class
+        public string Title { get; set; } // Gets and sets field Title, which will be used to hold the user input which will then be displayed as a list item
+        public bool IsDone { get; set; } // Gets and sets field IsDone, which will be used to track whether a user has selected the checkbox for individual list items
+    } // ends content of the class
+
